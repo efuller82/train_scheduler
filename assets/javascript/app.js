175fb082af2733b787d5324ba455433258228ebc
Eric Fuller <efuller82@gmail.com>
2019-07-09 12:23:22 -0500

    all cdn's added, linked to firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyD2BEuc9QNWR-l-RkyhLlq8XFmAM_3JM6I",
+    authDomain: "train-scheduler-a335d.firebaseapp.com",
+    databaseURL: "https://train-scheduler-a335d.firebaseio.com",
+    projectId: "train-scheduler-a335d",
+    storageBucket: "train-scheduler-a335d.appspot.com",
+    messagingSenderId: "122541575451",
+    appId: "1:122541575451:web:0f393d077c5faba8"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
